ebc433e2e322f412d9ad60413b26c9bc65d40d35
Chaitya Vora <90527884+vorachaitya@users.noreply.github.com>
2023-01-29 23:33:46 +0530

    smart contract deployed

---
+++ hardhat-tutorial/scripts/deploy.js
@@ -0,0 +1,38 @@
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+const { WHITELIST_CONTRACT_ADDRESS, METADATA_URL } = require("../constants");
+
+async function main() {
+  // Address of the whitelist contract that you deployed in the previous module
+  const whitelistContract = WHITELIST_CONTRACT_ADDRESS;
+  // URL from where we can extract the metadata for a Crypto Dev NFT
+  const metadataURL = METADATA_URL;
+
+  const cryptoDevsContract = await ethers.getContractFactory("CryptoDevs");
+
+  // deploy the contract
+  const deployedCryptoDevsContract = await cryptoDevsContract.deploy(
+    metadataURL,
+    whitelistContract
+  );
+
+  // Wait for it to finish deploying
+  await deployedCryptoDevsContract.deployed();
+
+  // print the address of the deployed contract
+  console.log(
+    "Crypto Devs Contract Address:",
+    deployedCryptoDevsContract.address
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
