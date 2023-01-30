c55e16fb23d6e20c00fe2abd48862e9313fef63e
Chaitya Vora <90527884+vorachaitya@users.noreply.github.com>
2023-01-30 13:15:26 +0530

    added url of hosted website

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,5 +1,5 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x33720D774adC2cAe74e7570b13222297B10e9FF5";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://an-ft.vercel.app/api/";
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
